3339fbfc6e5cd917222a2f242e287757722e153c
dejan1stefanov <dejan1stefanov@gmail.com>
2022-09-26 16:09:02 +0200

    Finished project

---
+++ src/components/Alan.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useContext } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { selectGenreOrCategory, searchMovie } from "../features/currentGenreOrCategory";
+import { fetchToken } from "../utils";
+import { ColorModeContext } from "../utils/ToggleColorMode";
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    alanBtn({
+      key: "6559c0269a7e201c8bb1cfdd20a90d8d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === "chooseGenre") {
+            const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+            if(foundGenre) {
+                history.push('/')
+                dispatch(selectGenreOrCategory(foundGenre.id))
+            } else {
+                const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+                history.pushState('/')
+                dispatch(selectGenreOrCategory(category))
+            }
+        } else if (command === "changeMode") {
+          if (mode === "light") {
+            setMode("light");
+          } else {
+            setMode("dark");
+          }
+        } else if (command === "login") {
+          fetchToken();
+        } else if (command === "logout") {
+          localStorage.clear();
+
+          history.push('/')
+        } else if(command === 'search') {
+            dispatch(searchMovie(query))
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
---
+++ src/services/TMDB.js
@@ -0,0 +1,81 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+
+export const tmdbApi = createApi({
+  reducerPath: "tmdbApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
+  endpoints: (builder) => ({
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+
+    //* Get Movies by [Type]
+    getMovies: builder.query({
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //* Get Movies by Search
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
+        //* Get Movies by Category
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "string"
+        ) {
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+        }
+
+        //* Get Movies by Genre
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "number"
+        ) {
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
+        //* Get Popular Movies
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
+
+    //* Get Movie
+    getMovie: builder.query({
+      query: (id) =>
+        `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
+
+    //* Get User Specific Lists
+    getList: builder.query({
+        query: ({ listName, accountId, sessionId, page }) => `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
+    }),
+
+    getRecommendations: builder.query({
+      query: ({ movie_id, list }) =>
+        `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+    }),
+
+    //* Get Actor Details
+    getActorsDetails: builder.query({
+      query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+    }),
+
+    //* Get Movies that an Actor plays a roll
+    getMoviesByActorId: builder.query({
+        query: ({id, page}) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
+    })
+  }),
+});
+
+export const {
+  useGetGenresQuery,
+  useGetMoviesQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+  useGetActorsDetailsQuery,
+  useGetMoviesByActorIdQuery,
+  useGetListQuery
+} = tmdbApi;
